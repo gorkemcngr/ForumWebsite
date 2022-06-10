87f80164461a0b07378c33caf1d5834d52890d6f
Gorkem <81441348+gorkemcngr@users.noreply.github.com>
2022-06-11 00:28:31 +0300

    message wrong date read fixed

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -88,7 +88,7 @@ namespace API.Data
                 {
                     foreach (var message in unreadMessages)
                     {
-                        message.DateRead = DateTime.UtcNow;
+                        message.DateRead = DateTime.Now;
                     }
                     await _context.SaveChangesAsync();
                 }
